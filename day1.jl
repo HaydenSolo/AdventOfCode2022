f90bb41142cd59e01827659dfc8fee5090512302
HaydenSolo <haydensolo@gmail.com>
2022-12-02 09:15:21 +1300

    Changed for to a foreach

--- day1.jl
+++ day1.jl
@@ -7,9 +7,8 @@ getcal(x::Elf) = x.calories
 f = open("input.txt", "r")
 lines = readlines(f)
 elves = Elf[Elf()]
-for line in lines
-    line == "" ? push!(elves, Elf()) : (elves[end].calories += parse(Int64, line))
-end
+
+foreach(line -> (line == "" ? push!(elves, Elf()) : (elves[end].calories += parse(Int64, line))), lines)
 
 highest = maximum(sort(getcal.(elves)))
 println(highest)
