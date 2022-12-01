f33b1baefdc36c138ee564228b66d03c29d797ff
HaydenSolo <haydensolo@gmail.com>
2022-12-02 09:07:41 +1300

    Tidy up a little bit

--- day1.jl
+++ day1.jl
@@ -2,17 +2,15 @@ mutable struct Elf
     calories::Int64
 end
 Elf() = Elf(0)
+getcal(x::Elf) = x.calories
 
 f = open("input.txt", "r")
 lines = readlines(f)
-elves = Elf[]
-push!(elves, Elf())
+elves = Elf[Elf()]
 for line in lines
     line == "" && push!(elves, Elf())
     line == "" || (elves[end].calories += parse(Int64, line))
 end
-
-getcal(x::Elf) = x.calories
 
 highest = maximum(sort(getcal.(elves)))
 println(highest)
